1b9eea9bb3b3e30fa61d342381de4b15ec3a740c
ahsanz024 <>
2020-04-24 19:29:56 +0200

    dashboard: added route-base/secret/login routes

--- sections/dashboard/src/pages/dashboard.js
+++ sections/dashboard/src/pages/dashboard.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Router } from '@reach/router';
 import Layout from '../components/layout';
+import Profile from '../components/profile';
+import RouteBase from '../components/route-base';
+import RouteSecret from '../components/route-secret';
+import RouteLogin from '../components/route-login';
 
 const Dashboard = () => {
   return (
     <Layout>
-      <p>TODO</p>
+      <Profile />
+      {/* Reach Router */}
+      <Router>
+        <RouteBase path="/dashboard/base"/>
+        <RouteSecret path="/dashboard/secret"/>
+        <RouteLogin path="/dashboard/login"/>
+      </Router>
+
     </Layout>
   )
 }
