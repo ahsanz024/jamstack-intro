e071e5219be93ff86568a3e5d535dc621cb0acab
Ahsan <ahsan024@gmail.com>
2020-05-10 16:39:36 +0200

    add: Basic dashboard routing

--- sections/dashboard/src/components/layout.js
+++ sections/dashboard/src/components/layout.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
+
 import './layout.css';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <IdentityContextProvider url="https://jamstack-intro-auth-ahsan.netlify.com">
       <header>
         <Link to="/">JAMStack App</Link>
       </header>
       <main>{children}</main>
-    </>
+    </IdentityContextProvider>
   )
 };
 
